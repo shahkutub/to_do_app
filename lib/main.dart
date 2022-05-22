a26fbee01e2f813609fa0e55d978d655ed6b9e39
shahkutub <shah.kutub1@gmail.com>
2022-05-22 18:16:05 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'view/home_page.dart';
+
+main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: HomePage(),
+    );
+  }
+}
